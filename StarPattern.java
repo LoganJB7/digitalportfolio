8bee27e9a6da3aad741185fd2b854d2a83c372d1
LoganJB7 <ljoeldalson63@gmail.com>
2024-07-24 18:38:28 -0500

    Add files via upload
    projects from school

---
+++ StarPattern.java
@@ -0,0 +1,17 @@
+/* This project consists of a pattern called stars that is being printed 
+in a specific order,
+*/
+
+public class StarPattern
+{
+   public static void main(String[] args)
+   {
+      System.out.println("   *");
+      System.out.println("  ***");
+      System.out.println(" *****");
+      System.out.println("*******");
+      System.out.println(" *****");
+      System.out.println("  ***");
+      System.out.println("   *");
+   }
+}
